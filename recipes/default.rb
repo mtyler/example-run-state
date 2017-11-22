b905b62f08e902818bc3c44b4e6bf9dc191dbf2c
Mike Tyler <michael.tyler@gmail.com>
2017-11-22 13:14:44 -0500

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+ruby_block "get svc name" do
+  block do
+    node.run_state['svcname'] = "some new value"
+    puts "\nService name found --->> " + node.run_state['svcname']
+  end
+end
